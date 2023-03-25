fe3af7613d5bf1fd17be54959028cae441eef7e8
mario <mario.aboujamra@weblib.eu>
2023-03-25 17:17:26 +0100

    add toolbar component

--- vva-gacti/src/app/app-routing.module.ts
+++ vva-gacti/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardAnimationComponent } from './modules/card-animation/card-animation.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'animation',
+  component: CardAnimationComponent,
+},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
